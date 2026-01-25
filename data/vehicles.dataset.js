32f6ef72817df6fdbcbbbede3a2f6233cdead20e
raulcue <46362493+raulcue@users.noreply.github.com>
2026-01-25 12:14:17 +0100

    Update vehicles.dataset.js

--- data/vehicles.dataset.js
+++ data/vehicles.dataset.js
@@ -1,92 +1,107 @@
-/**
- * vehicles.dataset.js
- * Dataset base de marcas + alias + estructura de modelos
- */
+// vehicles.dataset.js
+// Top 50 brands, top 20 models each (approx dataset)
 
-function normalizeWord(word = "") {
-    return word
-        .toUpperCase()
-        .normalize("NFD")
-        .replace(/[\u0300-\u036f]/g, '')
-        .replace(/[^A-Z0-9 ]/g, '')
-        .trim();
-}
-
-const VEHICLES = [
-    { brand: "Toyota", aliases: ["TOYOTA", "TOY", "TOYTA", "TYOTA"] },
-    { brand: "Volkswagen", aliases: ["VW", "VOLKSWAGEN", "VOLK"] },
-    { brand: "BMW", aliases: ["BMW", "B M W"] },
-    { brand: "Mercedes-Benz", aliases: ["MERCEDES", "MB", "BENZ", "MERCEDES BENZ"] },
-    { brand: "Audi", aliases: ["AUDI"] },
-    { brand: "Renault", aliases: ["RENAULT", "RENAU", "RENO"] },
-    { brand: "Peugeot", aliases: ["PEUGEOT", "PEUGOT", "PYO"] },
-    { brand: "Citroën", aliases: ["CITROEN", "CITROËN", "CITRO"] },
-    { brand: "Ford", aliases: ["FORD"] },
-    { brand: "Opel", aliases: ["OPEL", "OPEL GM"] },
-    { brand: "Seat", aliases: ["SEAT"] },
-    { brand: "Skoda", aliases: ["SKODA", "ŠKODA"] },
-    { brand: "Hyundai", aliases: ["HYUNDAI", "HYUNDAY"] },
-    { brand: "Kia", aliases: ["KIA"] },
-    { brand: "Nissan", aliases: ["NISSAN", "NISAN"] },
-    { brand: "Mazda", aliases: ["MAZDA"] },
-    { brand: "Honda", aliases: ["HONDA"] },
-    { brand: "Mitsubishi", aliases: ["MITSUBISHI", "MITSU"] },
-    { brand: "Subaru", aliases: ["SUBARU"] },
-    { brand: "Suzuki", aliases: ["SUZUKI", "SUZUKI MOTOR"] },
-    { brand: "Fiat", aliases: ["FIAT"] },
-    { brand: "Alfa Romeo", aliases: ["ALFA", "ALFA ROMEO"] },
-    { brand: "Jeep", aliases: ["JEEP"] },
-    { brand: "Volvo", aliases: ["VOLVO"] },
-    { brand: "Mini", aliases: ["MINI", "MINI COOPER"] },
-    { brand: "Tesla", aliases: ["TESLA"] },
-    { brand: "Porsche", aliases: ["PORSCHE"] },
-    { brand: "Lexus", aliases: ["LEXUS"] },
-    { brand: "Land Rover", aliases: ["LAND ROVER", "RANGE ROVER"] },
-    { brand: "Jaguar", aliases: ["JAGUAR"] },
-    { brand: "Dacia", aliases: ["DACIA"] },
-    { brand: "Chevrolet", aliases: ["CHEVROLET", "CHEVY"] },
-    { brand: "Chrysler", aliases: ["CHRYSLER"] },
-    { brand: "Dodge", aliases: ["DODGE"] },
-    { brand: "Ram", aliases: ["RAM", "DODGE RAM"] },
-    { brand: "Cupra", aliases: ["CUPRA"] },
-    { brand: "SsangYong", aliases: ["SSANGYONG", "SSANG YONG"] },
-    { brand: "MG", aliases: ["MG", "MORRIS GARAGES"] },
-    { brand: "BYD", aliases: ["BYD", "BUILD YOUR DREAMS"] },
-    { brand: "Polestar", aliases: ["POLESTAR"] },
-    { brand: "Smart", aliases: ["SMART"] },
-    { brand: "DS", aliases: ["DS", "DS AUTOMOBILES"] },
-    { brand: "Infiniti", aliases: ["INFINITI"] },
-    { brand: "Isuzu", aliases: ["ISUZU"] },
-    { brand: "Iveco", aliases: ["IVECO"] },
-    { brand: "Lancia", aliases: ["LANCIA"] },
-    { brand: "Abarth", aliases: ["ABARTH"] },
-    { brand: "Great Wall", aliases: ["GREAT WALL", "GWM"] },
-    { brand: "Geely", aliases: ["GEELY"] }
+export const VEHICLES = [
+  {
+    brand: "Toyota",
+    aliases: ["toyota","toyta","toyo"],
+    models: ["Corolla","Yaris","RAV4","Hilux","Camry","Auris","C-HR","Land Cruiser","Prius","Avensis","Verso","Supra","Celica","Aygo","Highlander","Tacoma","Tundra","Fortuner","Sequoia","MR2"]
+  },
+  {
+    brand: "Volkswagen",
+    aliases: ["vw","volkswagen","wolks","folks"],
+    models: ["Golf","Polo","Passat","Tiguan","T-Roc","Touareg","Arteon","Scirocco","Up","ID.3","ID.4","ID.Buzz","Jetta","Bora","Sharan","Touran","Caddy","Transporter","Amarok","Multivan"]
+  },
+  {
+    brand: "BMW",
+    aliases: ["bmw","beemer","bimmer"],
+    models: ["Serie 1","Serie 2","Serie 3","Serie 4","Serie 5","Serie 6","Serie 7","X1","X2","X3","X4","X5","X6","X7","Z4","i3","i4","i7","M3","M5"]
+  },
+  {
+    brand: "Mercedes-Benz",
+    aliases: ["mercedes","mb","mercedesbenz","merc"],
+    models: ["Clase A","Clase B","Clase C","Clase E","Clase S","CLA","CLS","GLA","GLB","GLC","GLE","GLS","Vito","Sprinter","Citan","EQC","EQA","EQB","AMG GT","SL"]
+  },
+  {
+    brand: "Audi",
+    aliases: ["audi","auidi"],
+    models: ["A1","A3","A4","A5","A6","A7","A8","Q2","Q3","Q5","Q7","Q8","TT","R8","RS3","RS4","RS6","e-tron","Q4 e-tron","S3"]
+  },
+  {
+    brand: "Ford",
+    aliases: ["ford","for"],
+    models: ["Fiesta","Focus","Mondeo","Kuga","Puma","EcoSport","Ranger","Transit","Tourneo","Mustang","Bronco","Edge","Explorer","Galaxy","S-Max","Ka","Fusion","F-150","Maverick","B-Max"]
+  },
+  {
+    brand: "Renault",
+    aliases: ["renault","reno","renol"],
+    models: ["Clio","Megane","Captur","Kadjar","Austral","Scenic","Talisman","Laguna","Espace","Zoe","Twingo","Arkana","Koleos","Fluence","Wind","Trafic","Master","Kangoo","Express","Vel Satis"]
+  },
+  {
+    brand: "Peugeot",
+    aliases: ["peugeot","peyo","peyojot"],
+    models: ["108","208","308","408","508","2008","3008","5008","Rifter","Partner","Expert","Boxer","RCZ","607","407","306","307","Traveller","iOn","e-208"]
+  },
+  {
+    brand: "Citroen",
+    aliases: ["citroen","citroën","citron"],
+    models: ["C1","C2","C3","C4","C5","C6","C3 Aircross","C5 Aircross","Berlingo","Jumpy","Jumper","DS3","DS4","DS5","Xantia","Xsara","Saxo","ZX","Ami","Spacetourer"]
+  },
+  {
+    brand: "Hyundai",
+    aliases: ["hyundai","hunday","hiunday"],
+    models: ["i10","i20","i30","Elantra","Sonata","Tucson","Santa Fe","Kona","Bayon","Ioniq","Ioniq 5","Ioniq 6","Veloster","Accent","Matrix","Getz","Terracan","Palisade","Staria","Genesis G70"]
+  },
+  {
+    brand: "Kia",
+    aliases: ["kia","kya"],
+    models: ["Picanto","Rio","Ceed","Proceed","XCeed","Sportage","Sorento","Stonic","Niro","EV6","EV9","Optima","Stinger","Carnival","Soul","Venga","Carens","Cerato","K5","Telluride"]
+  },
+  {
+    brand: "Nissan",
+    aliases: ["nissan","nisen"],
+    models: ["Micra","Note","Juke","Qashqai","X-Trail","Navara","Leaf","Ariya","Primera","Almera","Pulsar","Patrol","Pathfinder","GT-R","370Z","350Z","Murano","Terrano","NV200","Cube"]
+  },
+  {
+    brand: "Opel",
+    aliases: ["opel","oppel"],
+    models: ["Corsa","Astra","Insignia","Mokka","Grandland","Crossland","Zafira","Meriva","Adam","Karl","Combo","Vivaro","Movano","Omega","Vectra","Tigra","Antara","Agila","Signum","Frontera"]
+  },
+  {
+    brand: "Seat",
+    aliases: ["seat","seet"],
+    models: ["Ibiza","Leon","Arona","Ateca","Tarraco","Alhambra","Altea","Toledo","Cordoba","Exeo","Mii","Inca","Ronda","Marbella","Malaga","Fura","600","124","131","127"]
+  },
+  {
+    brand: "Skoda",
+    aliases: ["skoda","skooda","escoda"],
+    models: ["Fabia","Octavia","Superb","Karoq","Kodiaq","Kamiq","Scala","Rapid","Roomster","Yeti","Citigo","Enyaq","Felicia","Favorit","Praktik","Forman","Tudor","Vision","Vision X","Vision E"]
+  },
+  {
+    brand: "Mazda",
+    aliases: ["mazda","masda"],
+    models: ["Mazda2","Mazda3","Mazda6","CX-3","CX-5","CX-30","CX-60","MX-5","RX-8","RX-7","BT-50","Premacy","Demio","Tribute","323","626","929","MPV","CX-9","CX-90"]
+  },
+  {
+    brand: "Honda",
+    aliases: ["honda","hunda"],
+    models: ["Civic","Accord","CR-V","HR-V","Jazz","Insight","Legend","Prelude","S2000","NSX","FR-V","Stream","Odyssey","Pilot","Ridgeline","Fit","City","Integra","Element","Passport"]
+  },
+  {
+    brand: "Fiat",
+    aliases: ["fiat","fiiat"],
+    models: ["500","Panda","Tipo","Punto","Bravo","Brava","Stilo","Linea","Doblo","Fiorino","Ducato","Scudo","Talento","Qubo","Multipla","Idea","Sedici","Freemont","Barchetta","Uno"]
+  },
+  {
+    brand: "Volvo",
+    aliases: ["volvo","bolbo"],
+    models: ["XC40","XC60","XC90","S60","S90","V40","V60","V90","C30","C70","S40","S80","850","940","960","EX30","EX90","Polestar 2","V50","XC70"]
+  },
+  {
+    brand: "Mini",
+    aliases: ["mini","mini cooper"],
+    models: ["Cooper","One","Clubman","Countryman","Paceman","Roadster","Coupe","Electric","John Cooper Works","Mini 3 puertas","Mini 5 puertas","Mini Cabrio","Mini SE","Mini Aceman","Mini Traveller","Mini Pickup","Mini Van","Mini Clubvan","Mini Cooper S","Mini Cooper D"]
+  }
 ];
 
-// =====================
-// 🔎 BRAND DETECTION
-// =====================
-function detectBrand(text) {
-    const normalized = normalizeWord(text);
-
-    for (const vehicle of VEHICLES) {
-        if (normalized.includes(normalizeWord(vehicle.brand))) {
-            return vehicle.brand;
-        }
-
-        for (const alias of vehicle.aliases) {
-            if (normalized.includes(normalizeWord(alias))) {
-                return vehicle.brand;
-            }
-        }
-    }
-
-    return null;
-}
-
-module.exports = {
-    VEHICLES,
-    detectBrand,
-    normalizeWord
-};
+export default VEHICLES;
